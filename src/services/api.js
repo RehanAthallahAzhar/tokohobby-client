030171ab8aa79b36090262518691742a3603d552
RehanAthallahAzhar <azharrehan208@gmail.com>
2025-11-17 21:54:06 +0700

    feat: api for vps

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,37 @@
 import axios from 'axios';
 
+const getAuthToken = () => {
+    try {
+        const authStorage = localStorage.getItem('auth-storage'); 
+        if (!authStorage) {
+            return null;
+        }
+
+        const { state } = JSON.parse(authStorage);
+        return state.token || null;
+    } catch (error) {
+        console.error("Could not parse auth token:", error);
+        return null;
+    }
+};
+
 const addAuthToken = (config) => {
-    const token = localStorage.getItem('authToken');
+    const token = getAuthToken(); 
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
 };
 
+
 export const accountApi = axios.create({
-    baseURL: 'http://localhost:8081/api/v1/accounts',
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
+
 export const productApi = axios.create({
-    baseURL: 'http://localhost:8082/api/v1/products',
     headers: {
         'Content-Type': 'application/json',
     },
@@ -24,7 +39,6 @@ export const productApi = axios.create({
 productApi.interceptors.request.use(addAuthToken);
 
 export const cartApi = axios.create({
-    baseURL: 'http://localhost:8082/api/v1/cart',
     headers: {
         'Content-Type': 'application/json',
     },
@@ -32,9 +46,49 @@ export const cartApi = axios.create({
 cartApi.interceptors.request.use(addAuthToken);
 
 export const orderApi = axios.create({
-    baseURL: 'http://localhost:8083/api/v1/orders',
     headers: {
         'Content-Type': 'application/json',
     },
 });
 orderApi.interceptors.request.use(addAuthToken);
+
+
+
+// const addAuthToken = (config) => {
+//     const token = localStorage.getItem('authToken');
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// };
+
+// export const accountApi = axios.create({
+//     baseURL: 'http://localhost:8081/api/v1/accounts',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+// });
+
+// export const productApi = axios.create({
+//     baseURL: 'http://localhost:8082/api/v1/products',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+// });
+// productApi.interceptors.request.use(addAuthToken);
+
+// export const cartApi = axios.create({
+//     baseURL: 'http://localhost:8082/api/v1/cart',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+// });
+// cartApi.interceptors.request.use(addAuthToken);
+
+// export const orderApi = axios.create({
+//     baseURL: 'http://localhost:8083/api/v1/orders',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+// });
+// orderApi.interceptors.request.use(addAuthToken);
